e56b7ca19e0b23125762237e134e67c4c3c90059
Marco Muller <marco.muller.mm@gmail.com>
2022-04-13 16:05:45 +0200

    rent model added

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
+
+//variables d'environement et database
+require("dotenv").config({ path: "./config/.env" });
+require("./config/database");
 const express = require("express");
 const bodyParser = require("body-parser");
 const userRoutes = require("./routes/user.routes");
@@ -5,10 +9,6 @@ const materialRoutes = require("./routes/material.routes");
 const session = require("express-session");
 
 const UserModel = require("./Models/user.model");
-
-//variables d'environement et database
-require("dotenv").config({ path: "./config/.env" });
-require("./config/database");
 
 const app = express();
 
--- routes/material.routes.js
+++ routes/material.routes.js
@@ -1,10 +1,15 @@
 const router = require("express").Router();
 const MaterialController = require("../controllers/material.controller");
+const RentController = require("../controllers/rent.controller")
 
 router.get("/", MaterialController.getAllMaterials);
 router.get("/:id", MaterialController.getOneMaterial);
 router.put("/:id", MaterialController.updateMaterial);
 router.delete("/:id", MaterialController.deleteMaterial);
+router.get("/new")
 router.post("/new", MaterialController.newMaterial);
+
+router.get("/:id/new-rent");
+router.post("/:id/new-rent", RentController.newRent);
 
 module.exports = router;
