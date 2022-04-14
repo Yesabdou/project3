a0077c3ccb49c3d514790503b452d5804a7fdc2c
Marco Muller <marco.muller.mm@gmail.com>
2022-04-14 10:55:40 +0200

    rent routes and controllers

--- routes/material.routes.js
+++ routes/material.routes.js
@@ -9,7 +9,11 @@ router.delete("/:id", MaterialController.deleteMaterial);
 router.get("/new")
 router.post("/new", MaterialController.newMaterial);
 
+// rent routes
 router.get("/:id/new-rent");
-router.post("/:id/new-rent", RentController.newRent);
+router.post("/:id/new-rent", RentController.addNewRent);
+router.get("/:id/all-rents", RentController.seeAllRents);
+router.get("/:id/rent/:id", RentController.seeOneRent);
+router.patch("/:id/rent/:id", RentController.updateOneRent);
 
 module.exports = router;
