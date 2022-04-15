ad517a51e640d825b49847709565dccf3a2e701b
yesabdou <abdou.hamdaoui@icloud.com>
2022-04-15 10:24:21 +0200

    seed ok avec temps de 500ms

--- seeds/user.seed.js
+++ seeds/user.seed.js
@@ -9,7 +9,7 @@ const users = [
     pseudo: "À chacun ses vacances",
     finess: "497556851",
     email: "asso1@hotmail.fr",
-    adresse: "139 rue des Pyrénées - Bagnères 93 - 75020 Paris ",
+    adresse: "139 rue des Pyrénées 75020 Paris ",
     phone: "01 43 70 45 31",
     latitude: "",
     longitude: "",
@@ -23,7 +23,7 @@ const users = [
     pseudo: "ASMF – Association Handisport de Paris",
     finess: "497895852",
     email: "asso2@hotmail.fr",
-    adresse: "33, rue de la Roquette - Bat C2 - 75011 Paris ",
+    adresse: "33, rue de la Roquette75011 Paris ",
     phone: "06 12 73 90 42",
     latitude: "",
     longitude: "",
@@ -33,26 +33,12 @@ const users = [
     bio: "Porter la voix des usagers. Informer, conseiller les usagers, former les représentants des usagers. Porter un plaidoyer sur les droits des patients.",
     wishlit: "",
   },
-  {
-    pseudo:
-      "CATRED -Collectif des Accidentés du Travail, handicapés et Retraités pour l’Égalité des Droits",
-    finess: "497895853",
-    email: "asso3@hotmail.fr",
-    adresse: "20, boulevard Voltaire - 75011 Paris ",
-    phone: "01 40 21 38 11",
-    latitude: "",
-    longitude: "",
-    password: "azerty",
-    picture:
-      "https://w7.pngwing.com/pngs/682/603/png-transparent-grant-stairlift-funding-loan-disabled-child-child-loan-cartoon.png",
-    bio: "Porter la voix des usagers. Informer, conseiller les usagers, former les représentants des usagers. Porter un plaidoyer sur les droits des patients.",
-    wishlit: "",
-  },
+
   {
     pseudo: "FMH-UD de Paris – Fédération des Malades et Handicapés",
     finess: "497588954",
     email: "asso4@hotmail.fr",
-    adresse: "58 rue de Merlin - 75011 Paris ",
+    adresse: "58 rue de Merlin 75011 Paris ",
     phone: "01 43 55 97 74",
     latitude: "",
     longitude: "",
@@ -66,7 +52,7 @@ const users = [
     pseudo: "Advocacy Paris Île de France",
     finess: "497589855",
     email: "asso5@hotmail.fr",
-    adresse: "5 place des Fêtes - 75019 Paris ",
+    adresse: "5 place des Fêtes 75019 Paris ",
     phone: "01 46 07 18 18",
     latitude: "",
     longitude: "",
@@ -80,7 +66,7 @@ const users = [
     pseudo: "CoActis Santé",
     finess: "497895856",
     email: "asso6@hotmail.fr",
-    adresse: "85 boulevard de Port Royal - 75013 Paris ",
+    adresse: "85 boulevard de Port Royal 75013 Paris ",
     phone: "",
     latitude: "",
     longitude: "",
@@ -91,10 +77,10 @@ const users = [
     wishlit: "",
   },
   {
-    pseudo: "CRéDAVIS",
+    pseudo: "CREDAVIS",
     finess: "497589857",
     email: "asso7@hotmail.fr",
-    adresse: "Chemin des Gravilliers - 75016 Paris ",
+    adresse: "Chemin des Gravilliers 75016 Paris ",
     phone: "01 42 88 84 06",
     latitude: "",
     longitude: "",
@@ -108,7 +94,7 @@ const users = [
     pseudo: "France Assos Santé Île-de-France",
     finess: "497590850",
     email: "asso8@hotmail.fr",
-    adresse: "28 boulevard Pereire - 75017 Paris ",
+    adresse: "28 boulevard Pereire 75017 Paris ",
     phone: "01 43 29 92 38",
     latitude: "",
     longitude: "",
@@ -120,13 +106,15 @@ const users = [
   },
 ];
 
-User.create(users)
-  .then((users) => {
+const createUsers = async () => {
+  try {
+    for (let user of users) {
+      await User.create(user);
+    }
     console.log(`Created ${users.length} users`);
-
-    // Once created, close the DB connection
     mongoose.connection.close();
-  })
-  .catch((err) =>
-    console.log(`An error occurred while adding users from the DB: ${err}`)
-  );
+  } catch (error) {
+    console.log(`An error occurred while adding users from the DB: ${error}`);
+  }
+};
+createUsers();
