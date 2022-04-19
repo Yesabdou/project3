54ee9bd98d7fbf93e0ce1a42b93e538fec283cf2
Marco Muller <marco.muller.mm@gmail.com>
2022-04-19 17:29:31 +0200

    updated cors

--- index.js
+++ index.js
@@ -16,7 +16,8 @@ const app = express();
 
 app.use(
   cors({
-    origin: ['http://localhost:3000']  
+    credentials: true,
+    origin: process.env.ORIGIN || 'http://localhost:3000' 
   })
 );
 
