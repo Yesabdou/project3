d25691eb2bf52ca007cc2454aa6fe3940cf7ac3d
Marco Muller <marco.muller.mm@gmail.com>
2022-04-20 18:43:01 +0200

    model update

--- Models/material.model.js
+++ Models/material.model.js
@@ -50,7 +50,7 @@ materialSchema.pre("save", async function (next) {
     let possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
 
     for (let i = 0; i < 5; i++)
-      text += possible.charAt(Math.floor(Math.random() * possible.length));
+      ref += possible.charAt(Math.floor(Math.random() * possible.length));
 
     return ref;
   };
