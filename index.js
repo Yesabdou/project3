49e9bd9a282d8e9d9cd70153784759f6560d20b6
Marco Muller <marco.muller.mm@gmail.com>
2022-04-20 12:56:51 +0200

    cors modification

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ const app = express();
 app.use(
   cors({
     credentials: true,
-    origin: process.env.ORIGIN || 'http://localhost:3000' 
+    origin: ['http://localhost:3000' , "https://handishare.netlify"]
   })
 );
 
