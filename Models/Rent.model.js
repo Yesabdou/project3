e126a70f18f4739725a8ef0664c0ffd9905b308b
Marco Muller <marco.muller.mm@gmail.com>
2022-04-21 17:13:43 +0200

    update to rent model

--- Models/Rent.model.js
+++ Models/Rent.model.js
@@ -1,51 +1,50 @@
 const mongoose = require("mongoose");
 const Schema = require("mongoose");
 const MaterialModel = require("../Models/material.model");
-const UserModel = require("../Models/user.model")
+const UserModel = require("../Models/user.model");
 
 const rentSchema = new mongoose.Schema(
-{
-    materialId : {
-        type: Schema.Types.ObjectId,
-        ref: "Material",
+  {
+    materialId: {
+      type: Schema.Types.ObjectId,
+      ref: "Material",
     },
-    
+
     ownerId: {
-        type: Schema.Types.ObjectId,
-        ref: "User",
+      type: Schema.Types.ObjectId,
+      ref: "User",
     },
-    
+
     startDate: {
-        type: Date,
-        default: Date.now,
-        required: true,
+      type: Date,
+      default: Date.now,
+      required: true,
     },
 
     endDate: {
-        type: Date,
-        default: "",
-        required: true,
+      type: Date,
+      default: "",
+      required: true,
     },
 
-    status: { 
-        type: String,
-        enum: ["En cours", "Rendu"],
-        default: "",
-        required: true,
+    status: {
+      type: String,
+      enum: ["En cours", "Rendu", "A venir"],
+      default: "A venir",
+      required: true,
     },
 
     rentedTo: {
-        type: String,
-        required: true,
+      type: String,
+      required: true,
     },
 
     adresse: {
-        type: String,
-        required: true,
-    }
-
-},
-{ timestamps: true }
+      type: String,
+      required: true,
+    },
+  },
+  { timestamps: true }
 );
 
 const RentModel = mongoose.model("Rent", rentSchema);
